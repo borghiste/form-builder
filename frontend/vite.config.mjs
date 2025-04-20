a715988895314f5a8a229dedaadd0ab89b111038
borghiste <158812327+borghiste@users.noreply.github.com>
2025-04-20 18:20:47 +0200

    initial commit

---
+++ frontend/vite.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './src',  // Imposta la directory principale del progetto
+  build: {
+    outDir: '../dist', // Imposta la cartella di output
+  },
+});
