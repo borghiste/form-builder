cdf79451e5a2e105da276ff474c90e41648fe3c2
borghiste <158812327+borghiste@users.noreply.github.com>
2025-04-20 20:47:26 +0200

    feat(theme): add theme for app style

---
+++ frontend/src/theme/theme.ts
@@ -0,0 +1,87 @@
+import { createTheme, Typography } from "@mui/material";
+
+;
+
+export const LightTheme = createTheme({
+  palette: {
+ 
+    
+    background: {
+      default: '#F2F2F2', 
+    },
+    cyan: {
+      main: '#3D71D9',
+      light: '#3D80D9',
+      dark: '#0540F2'
+    },
+    magenta: {
+      main: '#D92938',
+      dark: '#BF1717',
+    },
+    gray:{ 
+      main:'#87909B',
+      light: '#E9E7E9'
+    }
+  },
+  text: {
+    primary: '#3E3E42',
+    secondary: '#05080D'
+  },
+  components: {
+    MuiSvgIcon: {
+      styleOverrides: {
+        root: {
+          color: '#87909B'
+        }
+      }
+    },
+    MuiListItemText: {
+      styleOverrides: {
+        primary: {
+          color:  '#3E3E42',
+        }
+      }
+    }
+  }
+});
+
+
+
+export const DarkTheme = createTheme({
+ 
+   
+      palette: {
+        mode: 'dark',
+        primary: {
+          main: '#F2F2F2',
+        },
+        background: {
+          default: '#05080D',
+          paper: '#F2F2F2',
+        },
+        magenta: {
+          main: '#AB6262',
+          dark: '#8C1B24'
+        },
+
+        gray: {
+          main: '#1C2026',
+        },
+        text: {
+          primary: '#C7D2D9',
+          secondary: '#8C95A4'
+        },
+       
+
+        },
+        components: {
+          MuiListItemText: {
+            styleOverrides: {
+              primary: {
+                color: '#F2F2F2'
+              }
+            }
+          }
+        }
+        
+      })
