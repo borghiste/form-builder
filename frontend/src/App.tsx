7312e078c748a9716fca28ea1e6bd7f83dd5cb73
borghiste <158812327+borghiste@users.noreply.github.com>
2025-04-20 18:51:46 +0200

    feat(app): add layout to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './components/Layout';
 
 export default function App(){
     return(
         
         <BrowserRouter>
+            <Layout>
+
         <Routes>
-            
+
         </Routes>
+            </Layout>
         </BrowserRouter>
     )
 }
