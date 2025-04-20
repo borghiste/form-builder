40db477ed9608de1b4516659bd4789115078c2c9
borghiste <158812327+borghiste@users.noreply.github.com>
2025-04-20 18:48:43 +0200

    feat(layout): add base layout for App

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -2,7 +2,7 @@ import {Box} from '@mui/material';
 export default function Layout({children}){
 
     return(
-        <Box   sx={{bgcolor: 'background.default', height:'100svh'}}>
+        <Box   sx={{ height:'100svh'}}>
         <header/>
         {children}
         <footer/>
