568d558b7d10767b65948ee0358e67140fe440af
borghiste <158812327+borghiste@users.noreply.github.com>
2026-01-08 16:11:27 +0100

    fix(home): fix home page

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,28 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 //MUI
-import { Box, Typography } from "@mui/material";
+import { Box, ButtonGroup, Typography } from "@mui/material";
 // COMPONENTS
 import BasicButton from "../components/UI/BasicButton";
+import RegisterForm from "../components/UI/RegistrationForm";
 export default function Home(){
+  const [showRegisterForm, setShowRegisterForm ] = useState(false);
 
 
     return(
       
         <Box
+        variant='main'
         sx={{minHeight:'100vh', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
           
-      <Typography variant="h1" sx={{color:'text.primary'}} >
-        home
-      </Typography>
-      <Typography sx={{color:'text.primary', maxWidth:'30rem'}} >
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto est reprehenderit repellendus odit dolorem, nobis culpa exercitationem eveniet, voluptate inventore sed voluptates ex mollitia. Expedita iusto asperiores enim inventore provident.
+  <Box component={'div'} sx={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', gap:2, textAlign:'center'}}>
+
+      <Typography variant="h1" sx={{color:'text.primary', }}>
+Formitekt
       </Typography>
 
-      <Box component={'div'} sx={{display:'flex'}}>
-        <BasicButton text={'get started'} variant={'contained'} color={'cyan.dark'} href={'/login'}/>
-      </Box>
+      <Typography variant="h2" sx={{color:'text.secondary' }}>
+The ultimate form builder for your data management.
+      </Typography>
+
+      <ButtonGroup>
+      <BasicButton
+       text={'sign up'} color={'cyan.main'} textColor={'white'} onClick={() => setShowRegisterForm(true)}/>
+
+
+      </ButtonGroup>
+  </Box>
+  {showRegisterForm && <RegisterForm />}
         
        
+  
         </Box>
+
+        
     )
 }
